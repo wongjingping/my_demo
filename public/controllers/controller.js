9a0aa813fe0e73a744b2e7c22accb4545168be1a
wongjingping <jingpingw@ncs.com.sg>
2016-05-31 12:57:22 +0800

    [working] edit and delete converted to angular ui bootstrap modals [err] still having ng-repeat dupes error

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,15 +2,20 @@
 
 var demoMod = angular.module('demoApp', ['ui.bootstrap','ngTagsInput']);
 
+// main controller
 demoMod.controller('demoCtrl', ['$scope','$http','$uibModal',
 	function($scope, $http, $uibModal){
 
 		// static values
-		$scope.verticals = ['Commercial','Education','General','Government',
-			'Healthcare','Public Safety','Transport','Various'];
-
-
 		$http.defaults.headers.post["Content-Type"] = "application/json";
+
+
+		// Helper Functions
+
+		// copy an object (mainly for demo)
+		var copyObject = function(obj){
+			return JSON.parse(JSON.stringify(obj));
+		};
 
 		// capitalizes first letter
 		var toTitleCase = function(str){
@@ -33,6 +38,14 @@ demoMod.controller('demoCtrl', ['$scope','$http','$uibModal',
 			return tagInput;
 		};
 
+		// convert array to tag inputs
+		var array2TagInput = function(arr){
+			for (var i = 0; i < arr.length; i++) {
+				arr[i] = {"text":arr[i]};
+			};
+			return arr;
+		};
+
 
 		// data refresh
 		var refresh = function(){
@@ -41,30 +54,28 @@ demoMod.controller('demoCtrl', ['$scope','$http','$uibModal',
 					res.length + ' items');
 				$scope.demos = res;
 				$scope.newdemo = null;
-				// hackish way to prevent modal hanging page :|
-				$(".modal-backdrop").remove();
-				$("body").attr("class","modal-close");
 			});
 		};
 
-		// performs cleaning on demo
-		var cleanDemo = function(demo){
-			if(demo.hasOwnProperty('Creator')) {
+		// performs cleaning on demo for saving to mongoDB
+		var toMongoFields = function(demo){
+			if(demo['Creator'] != null) {
+				console.log('creator ' + demo['Creator']);
 				demo['Creator'] = toTitleCase(demo['Creator']);	
 			};
-			if(demo.hasOwnProperty('LastUpdated')) {
+			if(demo['LastUpdated'] != null) {
 				demo['LastUpdated'] = formatDate(demo['LastUpdated']);
-			}
+			};
 			
 			// check for array fields
-			if(!demo.hasOwnProperty('AnalyticTechniques')){
+			if(demo['AnalyticTechniques'] == null){
 				demo['AnalyticTechniques'] = [];
 			} else if (!demo['AnalyticTechniques'] === Array) {
 				console.log('Type of Analytics is not an Array!');
 			} else {
 				demo['AnalyticTechniques'] = tagInput2Array(demo['AnalyticTechniques']);
 			};
-			if(!demo.hasOwnProperty('Technology')){
+			if(demo['Technology'] == null){
 				demo['Technology'] = [];
 			} else if (!demo['Technology'] === Array) {
 				console.log('Technology is not an Array!');
@@ -74,67 +85,148 @@ demoMod.controller('demoCtrl', ['$scope','$http','$uibModal',
 			return demo;
 		};
 
-		$scope.addDemo = function(newdemo){
+		// convert JSON data from mongoDB into field types
+		var toModalFields = function(demo){
+			// convert to date
+			if(demo.hasOwnProperty('LastUpdated')) {
+				demo['LastUpdated'] = new Date(demo['LastUpdated']);
+			};
+			// check for array fields
+			if(!demo.hasOwnProperty('AnalyticTechniques')){
+				demo['AnalyticTechniques'] = [];
+			} else if (!demo['AnalyticTechniques'] === Array) {
+				console.log('Type of Analytics is not an Array!');
+			} else {
+				demo['AnalyticTechniques'] = array2TagInput(demo['AnalyticTechniques']);
+			};
+			if(!demo.hasOwnProperty('Technology')){
+				demo['Technology'] = [];
+			} else if (!demo['Technology'] === Array) {
+				console.log('Technology is not an Array!');
+			} else {
+				demo['Technology'] = array2TagInput(demo['Technology']);
+			};
+			console.log('toModalFields');
+			console.log(demo);
+			return demo;
+		}
+
+
+		var addDemo = function(newdemo){
 			console.log('adding');
-			newdemo = cleanDemo(newdemo);
+			newdemo = toMongoFields(newdemo);
 			$http.post('/demolist', newdemo).success(function(res){
-				console.log('rcv from db:');
+				console.log('POST rcv from db:');
 				console.log(res);
 				refresh();
 			});
 		};
 
-		$scope.editDemo = function(demo){
+		var editDemo = function(demo){
 			console.log('editing');
-			console.log(demo);
-			// cleanDemo(demo);
+			demo = toMongoFields(demo);
+			// swap the existing data in $scope with updated demo entry
+			for (var i = 0; i < $scope.demos.length; i++) {
+				if ($scope.demos[i]._id == demo._id) {
+					console.log('matched id ' + demo._id);
+					$scope.demos[i] = demo;
+				};
+			};
 			// alternative syntax for a http request
 			$http({
 				url:'/demolist/' + demo._id,
 				method: 'PUT',
 				data: demo
 			}).success(function(res){
-				console.log('rcv from db:');
+				console.log('PUT rcv from db:');
 				console.log(res);
 			})
-			// $scope.refresh();
 		}
 
-		$scope.removeDemo = function(demo){
-			var winRemove = window.confirm(
-				'Are you sure you want to remove use case ' + 
-				demo['UseCase'] + '?');
-			if (winRemove == true) {
-				console.log('removing ' + demo._id);
-				$http.delete('/demolist/' + demo._id);
-				window.alert(demo['UseCase'] + ' removed!');
-				refresh();
-			}
-
-		};
-
-		// modal stuff
-		$scope.openModal = function(){
+		// open modal for adding a new entry
+		$scope.openAddModal = function(){
 			var modalInstance = $uibModal.open({
 				animation: true,
-				templateUrl: 'views/modal.html',
+				templateUrl: 'views/modal_fields.html',
 				controller: 'modalCtrl',
 				resolve: {
-					verticals: function(){
-						return $scope.verticals;
+					operation: function(){
+						return 'Add';
+					},
+					demo: function(){
+						return null;
 					}
 				}
 			});
 
 			// refresh data whenever the modal is closed
 			modalInstance.closed.then(function(data){
+				console.log('add modal closed');
 				refresh();
 			});
 
 			// add newdemo upon closing the modal and receiving the object
 			modalInstance.result.then(function(newdemo){
 				console.log('closed modal with newdemo');
-				$scope.addDemo(newdemo);
+				addDemo(newdemo);
+			});
+		};
+
+		// open modal for editing an existing entry
+		$scope.openEditModal = function(demo_i){
+			var modalInstance = $uibModal.open({
+				animation: true,
+				templateUrl: 'views/modal_fields.html',
+				controller: 'modalCtrl',
+				resolve: {
+					operation: function(){
+						return 'Edit';
+					},
+					demo: function(){
+						// copy demo to prevent 'live' updating in faded bg
+						var demo = copyObject(demo_i);
+						return toModalFields(demo);
+					}
+				}
+			});
+
+			// refresh data whenever the modal is closed
+			modalInstance.closed.then(function(demo){
+				console.log('edit modal closed');
+				refresh();
+			});
+
+			// update demo upon closing the modal and receiving the object
+			modalInstance.result.then(function(demo){
+				console.log('closed modal with updated demo');
+				editDemo(demo);
+			});
+		};
+
+		// open modal for deleting an existing entry
+		$scope.openRemoveModal = function(demo){
+			var modalInstance = $uibModal.open({
+				animation: true,
+				templateUrl: 'views/modal_confirm.html',
+				controller: 'modalRemoveCtrl',
+				resolve: {
+					demo: function(){
+						return demo;
+					}
+				}
+			});
+
+			// delete demo from view upon closing the modal and receiving the object
+			modalInstance.result.then(function(id){
+				console.log('closed modal and delete id ' + id);
+				$http.delete('/demolist/' + id);
+				// or replace with refresh?
+				for (var i = 0; i < $scope.demos.length; i++) {
+					if ($scope.demos[i]._id == id) {
+						$scope.demos.splice(i, i+1);
+						break;
+					};
+				};
 			});
 		};
 
@@ -145,13 +237,13 @@ demoMod.controller('demoCtrl', ['$scope','$http','$uibModal',
 }]);
 
 
-// controller for modal
+// controller for add/edit modal
 demoMod.controller('modalCtrl', 
-	function($scope, $http, $uibModalInstance, verticals){
+	function($scope, $uibModalInstance, operation, demo){
 
-	// static values
-	$scope.verticals = verticals;
-	// tags-input options
+	// static tags-input options
+	$scope.verticals = ['Commercial','Education','General','Government',
+	'Healthcare','Public Safety','Transport','Various'];
 	$scope.technology = ["R","Python","Shiny","Tableau","TIBCO Spotfire",
 	"Qlikview","SAS Visual Analytics","SAS Enterprise Miner",
 	"Microstrategy","Gephi","Java","Matlab"]
@@ -159,6 +251,9 @@ demoMod.controller('modalCtrl',
 	"Forecasting","Social Media Analytics","Optimization","Simulation",
 	"CEP","Clustering","Association Rules Mining","Network Analytics"];
 
+	// pass in demo details
+	$scope.operation = operation
+	$scope.demo = demo
 
 	// datepicker pop-up settings
 	$scope.popupDate = {
@@ -175,12 +270,27 @@ demoMod.controller('modalCtrl',
 		};
 	$scope.altInputFormats = ['M!/d!/yyyy'];
 
-	// close the modal and return newdemo object
-	$scope.addDemo = function(newdemo){
-		$uibModalInstance.close(newdemo);
+	// close the modal and return demo object
+	$scope.returnDemo = function(){
+		$uibModalInstance.close($scope.demo);
 	};
 
 	// dismiss the modal without result
+	$scope.cancel = function(){
+		$uibModalInstance.dismiss('cancel');
+	};
+});
+
+// controller for confirming deletion
+demoMod.controller('modalRemoveCtrl',
+	function($scope, $uibModalInstance, demo){
+
+	$scope.demo = demo;
+
+	$scope.removeDemo = function(){
+		$uibModalInstance.close($scope.demo._id);
+	};
+
 	$scope.cancel = function(){
 		$uibModalInstance.dismiss('cancel');
 	};
